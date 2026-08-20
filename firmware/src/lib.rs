db6fbc3c0282146d40dd26868e65d2cbf8ca612e
Samuel Spinn <samuel.spinn@gmail.com>
2026-08-20 16:52:42 +0100

    refactor: create missing STM HAL `use` statement
    Refs: #3

--- firmware/src/lib.rs
+++ firmware/src/lib.rs
@@ -3,9 +3,7 @@
 
 use defmt_rtt as _; // global logger
 
-// TODO(5) adjust HAL import
-// use some_hal as _; // memory layout
-
+use stm32f3xx_hal;
 use panic_probe as _;
 
 // same panicking *behavior* as `panic-probe` but doesn't print a panic message
