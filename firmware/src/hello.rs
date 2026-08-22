a1ab61e051a6a5046cfeac099c615b51a9a8815c
Samuel Spinn <samuel.spinn@gmail.com>
2026-08-22 10:02:39 +0100

    refactor(firmware): replace knurling rs example with embassy example
    - Embassy example:
    https://github.com/embassy-rs/embassy/tree/main/examples/stm32f3
    
    Refs #5

---
+++ firmware/src/hello.rs
@@ -0,0 +1,20 @@
+#![no_std]
+#![no_main]
+
+use defmt::info;
+use defmt_rtt as _;
+use embassy_executor::Spawner;
+use embassy_stm32::Config;
+use embassy_time::Timer;
+use panic_probe as _;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) -> ! {
+    let config = Config::default();
+    let _p = embassy_stm32::init(config);
+
+    loop {
+        info!("Hello World!");
+        Timer::after_secs(1).await;
+    }
+}
