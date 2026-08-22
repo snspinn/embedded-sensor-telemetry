3f2364e8f7e76933e808e2c34e955633b3e56151
Samuel Spinn <samuel.spinn@gmail.com>
2026-08-22 10:38:59 +0100

    feat(firmware): create blinky bin
    For now, only SE blue LED.
    
    Refs #5

---
+++ firmware/src/bin/blinky.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use defmt_rtt as _;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::Timer;
+use panic_probe as _;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+    info!("Hello World!");
+
+    let mut led = Output::new(p.PE12, Level::Low, Speed::Low); // SE, blue
+
+    loop {
+        info!("led on!");
+        led.set_high();
+        Timer::after_millis(500).await;
+
+        info!("led off!");
+        led.set_low();
+        Timer::after_millis(500).await;
+    }
+}
